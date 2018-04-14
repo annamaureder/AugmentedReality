b7cac5e172a545865468595eb44b65ec7f658911
Anna <anna.maureder@gmx.at>
2018-04-14 11:13:32 +0200

    Implement drag Atoms Script
    - GameObjects with tag "Atom" can be dragged over the scene
    - only x and z axis are effected --> y always stays stable (height)

--- AtomicAR/Assets/Scripts/DragAtoms.cs
+++ AtomicAR/Assets/Scripts/DragAtoms.cs
@@ -1,41 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DragAtoms : MonoBehaviour {
+public class DragAtoms : MonoBehaviour
+{
 	
 	private float dist;
 	private bool dragging = false;
 	private Transform atom;
 	private Vector3 pos3D;
+	private float planeY = 0;
+	private float height = 0.15f;
 
-	void Update() {
-
+	void Update ()
+	{
 		if (Input.touchCount != 1) {
 			dragging = false; 
 			return;
 		}
 
-		Touch touch = Input.touches[0];
+		Touch touch = Input.touches [0];
 		Vector2 pos = touch.position;
 
-		if(touch.phase == TouchPhase.Began) {
+		if (touch.phase == TouchPhase.Began) {
 			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(pos); 
-			if(Physics.Raycast(ray, out hit) && (hit.collider.tag == "Atom"))
-			{
-				Debug.Log ("Drag Atom");
+			Ray ray = Camera.main.ScreenPointToRay (pos); 
+			if (Physics.Raycast (ray, out hit) && (hit.collider.tag == "Atom")) {
 				atom = hit.transform;
-				dist = hit.transform.position.z - Camera.main.transform.position.z;
-				pos3D = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, dist));
 				dragging = true;
 			}
-		}
-		else if (dragging && touch.phase == TouchPhase.Moved) {
-			pos3D = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, dist));
-			atom.position = pos3D;
-		}
-		else if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
-			dragging = false;
+		} else if (dragging && touch.phase == TouchPhase.Moved) {
+			Ray ray = Camera.main.ScreenPointToRay (pos);
+			Plane plane = new Plane (Vector3.up, Vector3.up * planeY);
+			float distance;
+			if (plane.Raycast (ray, out distance)) {
+				atom.position = ray.GetPoint (distance);
+				atom.position = new Vector3 (atom.position.x, atom.position.y + height, atom.position.z);
+			}
 		}
 	}
 }
